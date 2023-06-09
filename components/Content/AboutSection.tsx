c9607e4b34b38146e7f9a74a1774cd80930013d2
Maruf Bepary <bepary71@gmail.com>
2023-06-09 19:32:03 +0100

    Updated about me

--- components/Content/AboutSection.tsx
+++ components/Content/AboutSection.tsx
@@ -51,35 +51,62 @@ const AboutSection = () => {
 
         <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
           {/* Left section */}
-          <div className="md:w-1/2 ">
+          <div className="md:w-1/2">
             <h1 className="text-center text-2xl font-bold mb-6 mt-6 md:text-left">
               Get to know me!
             </h1>
             <p>
-              Hey, my name is Maruf and I&#39;m a{" "}
-              <span className="font-bold">{"highly ambitious"}</span>,
-              <span className="font-bold">{" self-motivated"}</span>, and
-              <span className="font-bold">{" driven"}</span> software engineer
-              based in London, UK.
+              Hello there! I&#39;m Maruf, a{" "}
+              <span className="font-bold">software engineer</span> fueled by{" "}
+              <span className="font-bold">
+                curiosity, innovation, and an insatiable desire to explore the
+                depths of technology
+              </span>
+              . London, the city of rain, fish and chips, the iconic Big Ben,
+              and technology, has been my home base for the past few years.
+              Here, at the esteemed Royal Holloway University, I&#39;ve embarked
+              on an intellectual odyssey to pursue my BS in Computer Science.
             </p>
             <br />
             <p>
-              I will graduate from Royal Holloway University with a BS in
-              Computer Science. I have been working on various projects and
-              experimenting with different technologies to widen my knowledge.
+              Software engineering for me is akin to conducting an orchestra - a
+              <span className="font-bold"> symphony of codes</span> that come
+              together to create something remarkable. From developing APIs to
+              building intricate front-end architectures, I&#39;ve dabbled in
+              all parts of the{" "}
+              <span className="font-bold">software development lifecycle</span>.
             </p>
             <br />
             <p>
-              In addition to full stack development, I am also interested in
-              Machine Learning and Mathematics. I am currently learning more
-              advanced topics in Machine Learning, building upon what I learnt
-              in university. I am also learning DevOps through Udemy.
+              But don&#39;t be fooled, my technical interest doesn&#39;t stop at
+              full-stack development. My fascination for understanding the
+              &quot;why&quot; behind the &quot;what&quot; led me to{" "}
+              <span className="font-bold">Machine Learning</span>. To further
+              satiate my curiosity, I&#39;m currently digging deeper into the
+              advanced layers of Machine Learning, adding a valuable layer to my
+              skill set.
             </p>
             <br />
             <p>
-              I have a wide range of hobbies and passions that keep me busy;
-              these include playing football and badminton, reading Science,
-              learning about tech and traveling.
+              In the same vein, my passion for{" "}
+              <span className="font-bold">efficiency</span> and streamlining
+              operations has kindled my interest in{" "}
+              <span className="font-bold">DevOps</span>. As I engage with the
+              principles of continuous integration, testing, deployment and
+              monitoring through Udemy, I find myself increasingly intrigued by
+              the interplay of development and operations.
+            </p>
+            <br />
+            <p>
+              Away from the computer screen, I find myself diving into a
+              multitude of hobbies that bring balance to my life. Whether
+              it&#39;s showing off my skills in football and badminton,
+              immersing myself in science-based literature, or packing a bag to
+              discover a new city, I believe in{" "}
+              <span className="font-bold">
+                seizing the day and living life to the fullest
+              </span>
+              .
             </p>
           </div>
 
