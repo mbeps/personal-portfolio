f74666dc3c480ea8a4d382e07c4ec7bf7c9cbb58
Maruf Bepary <bepary71@gmail.com>
2023-06-10 18:24:37 +0100

    Created title component to avoid repetition

---
+++ components/Title.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface TitleProps {
+  title: string;
+}
+
+const Title: React.FC<TitleProps> = ({ title }) => {
+  return (
+    <h1 className="text-center font-bold text-4xl">
+      {title}
+      <hr className="w-6 h-1 mx-auto my-4 bg-red-500 border-0 rounded" />
+    </h1>
+  );
+};
+
+export default Title;
