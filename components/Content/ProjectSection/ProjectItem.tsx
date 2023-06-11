6731e31f1a7ca38e79f5d31fbc7b1d56576adbf3
Maruf Bepary <bepary71@gmail.com>
2023-06-11 14:47:53 +0100

    Added transition effect on project item - Before it would just switch making it look odd

--- components/Content/ProjectSection/ProjectItem.tsx
+++ components/Content/ProjectSection/ProjectItem.tsx
@@ -40,7 +40,7 @@ const ProjectItem: React.FC<Project> = ({
 
   return (
     <SlideUp offset="-300px 0px -300px 0px">
-      <div className="bg-neutral-100 dark:bg-stone-950 p-4 rounded-xl sm:bg-transparent sm:dark:bg-transparent sm:p-0">
+      <div className="bg-neutral-100 dark:bg-stone-950 p-4 rounded-xl sm:bg-transparent sm:dark:bg-transparent sm:p-0 transition-colors duration-500 ease-in-out">
         <div className="flex flex-col animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
           {imageURL && (
             <div className="md:w-1/2">
