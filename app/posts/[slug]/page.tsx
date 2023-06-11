a21cd7f5c37ccf8f5539a8d256a03a5c42c48ab3
Maruf Bepary <bepary71@gmail.com>
2023-06-11 19:09:41 +0100

    Created post page where user can read the post

---
+++ app/posts/[slug]/page.tsx
@@ -0,0 +1,39 @@
+import fs from "fs";
+import Markdown from "markdown-to-jsx";
+import matter from "gray-matter";
+import getPostMetadata from "@/actions/getPostMetadata";
+import Title from "@/components/Title";
+
+const getPostContent = (slug: string) => {
+  const folder = "posts/";
+  const file = `${folder}${slug}.md`;
+  const content = fs.readFileSync(file, "utf8");
+  const matterResult = matter(content);
+  return matterResult;
+};
+
+export const generateStaticParams = async () => {
+  const posts = getPostMetadata();
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+};
+
+const PostPage = (props: any) => {
+  const slug = props.params.slug;
+  const post = getPostContent(slug);
+  return (
+    <div>
+      <div className="my-12 text-center">
+        <Title title={post.data.title} />
+        <p className="text-slate-400 mt-2">{post.data.date}</p>
+      </div>
+
+      <article className="prose lg:prose-xl dark:prose-invert prose-img:rounded-lg">
+        <Markdown>{post.content}</Markdown>
+      </article>
+    </div>
+  );
+};
+
+export default PostPage;
