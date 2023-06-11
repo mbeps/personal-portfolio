6abdeb8a4ab233627528077cd02dbeb56dd3d2e0
Maruf Bepary <bepary71@gmail.com>
2023-06-11 12:18:50 +0100

    Added documentation for pages in the site

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ import AboutSection from "@/components/Content/AboutSection";
 import HeroSection from "@/components/Content/HeroSection";
 import ProjectsSection from "@/components/Content/ProjectSection/ProjectSection";
 
+/**
+ * Home page which contains the Hero, About and Projects sections.
+ * @returns (JSX.Element): Home page
+ */
 export default function Home() {
   return (
     <main>
