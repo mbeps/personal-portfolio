fa90a2bd34b9c90a3a4f74aa07d419025dbb31cd
Maruf Bepary <bepary71@gmail.com>
2023-06-11 20:38:34 +0100

    Revert "Display no posts message if there are no posts"
    This reverts commit 962aea2e35f48bee6d81ad38748c08dc681ee4c1.

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -9,17 +9,15 @@ export default function Posts() {
     <main>
       <section id="posts">
         <Title title="Posts" />
-        {postMetadata.length > 0 ? (
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-20">
-            {postMetadata.map((post) => (
-              <PostItem key={post.slug} {...post} />
-            ))}
-          </div>
-        ) : (
-          <div className="flex items-center justify-center my-36">
-            <h2 className="text-2xl font-bold">No posts</h2>
-          </div>
-        )}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-20">
+          {postMetadata ? (
+            postMetadata.map((post) => <PostItem key={post.slug} {...post} />)
+          ) : (
+            <div className="flex items-center justify-center h-screen">
+              <h2 className="text-2xl font-bold">No posts</h2>
+            </div>
+          )}
+        </div>
       </section>
     </main>
   );
