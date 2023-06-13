da798fee2026759fcdf02494b6361dcf5ab0d865
Maruf Bepary <bepary71@gmail.com>
2023-06-13 12:57:35 +0100

    Bug Fix: full post item is now clickable - Before only the title was clickable making it confusing when trying to open a post

--- components/Posts/PostItem.tsx
+++ components/Posts/PostItem.tsx
@@ -9,8 +9,9 @@ import React from "react";
  */
 const PostItem: React.FC<PostMetadata> = (props) => {
   return (
-    <div
-      className="
+    <Link href={`/posts/${props.slug}`}>
+      <div
+        className="
         border 
         cursor-pointer
         border-slate-300 dark:border-neutral-600
@@ -20,27 +21,26 @@ const PostItem: React.FC<PostMetadata> = (props) => {
         rounded-xl 
         shadow-md hover:shadow-sm
         transition-colors duration-700"
-    >
-      <p
-        className="
+      >
+        <p
+          className="
           text-sm 
           text-neutral-400 dark:text-neutral-500"
-      >
-        {props.date}
-      </p>
+        >
+          {props.date}
+        </p>
 
-      <Link href={`/posts/${props.slug}`}>
         <h2 className=" text-xl font-bold  mb-4 text-neutral-900 dark:text-neutral-100">
           {props.title}
         </h2>
-      </Link>
-      <p
-        className="
+        <p
+          className="
         text-neutral-700 dark:text-neutral-200"
-      >
-        {props.subtitle}
-      </p>
-    </div>
+        >
+          {props.subtitle}
+        </p>
+      </div>
+    </Link>
   );
 };
 
