c86e32acf7ab3edbd7fdd02db91e4c138b036274
Maruf Bepary <bepary71@gmail.com>
2023-06-13 12:54:52 +0100

    Added tab name

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
 
   return (
     <html lang="en" className={currentTheme === "dark" ? "dark" : ""}>
+      <title>Maruf</title>
       <head />
       <body
         className={`
