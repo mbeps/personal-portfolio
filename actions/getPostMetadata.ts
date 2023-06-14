a03b9f2aa722533f18a411e43f315ded3cf6bf2e
Maruf Bepary <bepary71@gmail.com>
2023-06-14 17:51:20 +0100

    Added option to hide some posts - This can be used to posts discussing what I learnt from projects

--- actions/getPostMetadata.ts
+++ actions/getPostMetadata.ts
@@ -4,6 +4,8 @@ import matter from "gray-matter";
 
 /**
  * Get the metadata for all posts stored in the posts/ directory.
+ * Does not return posts with display false.
+ * Undefined posts are still returned.
  * @returns {PostMetadata[]} An array of post metadata.
  */
 const getPostMetadata = (): PostMetadata[] => {
@@ -11,17 +13,19 @@ const getPostMetadata = (): PostMetadata[] => {
   const files = fs.readdirSync(folder);
   const markdownPosts = files.filter((file) => file.endsWith(".md"));
 
-  // Get gray-matter data from each file.
-  const posts = markdownPosts.map((fileName) => {
-    const fileContents = fs.readFileSync(`posts/${fileName}`, "utf8");
-    const matterResult = matter(fileContents);
-    return {
-      title: matterResult.data.title,
-      date: matterResult.data.date,
-      subtitle: matterResult.data.subtitle,
-      slug: fileName.replace(".md", ""),
-    };
-  });
+  const posts: PostMetadata[] = markdownPosts
+    .map((fileName) => {
+      const fileContents = fs.readFileSync(`posts/${fileName}`, "utf8");
+      const matterResult = matter(fileContents);
+      return {
+        title: matterResult.data.title,
+        date: matterResult.data.date,
+        subtitle: matterResult.data.subtitle,
+        slug: fileName.replace(".md", ""),
+        display: matterResult.data.display,
+      };
+    })
+    .filter((post) => post.display !== "false");
 
   return posts;
 };
