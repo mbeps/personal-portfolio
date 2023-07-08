94ba12cd8c15bde3d0edd20b8d9cd942b4c946fc
Maruf Bepary <bepary71@gmail.com>
2023-07-08 16:53:57 +0100

    Completed gallery component - Added transitions - Made width dynamic - Added documentation

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -7,7 +7,25 @@ const page: React.FC = () => {
   return (
     <div>
       <Gallery
-        images={["/projects/convo-gpt.png", "/projects/osmos-game.png"]}
+        images={[
+          "/projects/convo-gpt.png",
+          "/projects/osmos-game.png",
+          "/projects/circus-discussions-fyp.png",
+          "/projects/drumroll-music.png",
+          "/projects/noodle.png",
+          "/projects/ringmaster-messaging.png",
+          "/projects/sideshow-articles.png",
+          "/projects/circus-discussions-fyp.png",
+          "/projects/circus-discussions-fyp.png",
+
+          "/projects/drumroll-music.png",
+          "/projects/noodle.png",
+          "/projects/ringmaster-messaging.png",
+
+          "/projects/drumroll-music.png",
+          "/projects/noodle.png",
+          "/projects/ringmaster-messaging.png",
+        ]}
       />
     </div>
   );
--- components/Gallery/Gallery.tsx
+++ components/Gallery/Gallery.tsx
@@ -2,19 +2,39 @@
 
 import Image from "next/image";
 import React, { useState } from "react";
-import { FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
+import {
+  IoIosArrowDropleftCircle,
+  IoIosArrowDroprightCircle,
+} from "react-icons/io";
 
 interface GalleryProps {
   images: string[];
 }
 
+/**
+ * Gallery Component which allows users to view a list of images.
+ * At the top, there is a preview of the currently selected image.
+ * Bellow are the thumbnails of the available images.
+ * Clicking on a thumbnail will change the preview to that image.
+ * There are also buttons on the left and right of the preview to change the image.
+ * @param images (string[]) - List of image urls (relative or absolute
+ * @returns (JSX.Element) - Gallery Component
+ */
 const Gallery: React.FC<GalleryProps> = ({ images }) => {
   const [activeIndex, setActiveIndex] = useState(0);
 
+  /**
+   * Changes the active image to the next image in the list.
+   * If the current image is the last image, it will loop back to the first image.
+   */
   const handleNext = () => {
     setActiveIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
+  /**
+   * Changes the active image to the previous image in the list.
+   * If the current image is the first image, it will loop back to the last image.
+   */
   const handlePrev = () => {
     setActiveIndex(
       (prevIndex) => (prevIndex - 1 + images.length) % images.length
@@ -23,11 +43,18 @@ const Gallery: React.FC<GalleryProps> = ({ images }) => {
 
   return (
     <div className="flex flex-col items-center relative">
-      <FiArrowLeftCircle
-        className="absolute left-0 top-1/2 transform -translate-y-1/2 text-3xl cursor-pointer"
-        onClick={handlePrev}
-      />
-      <div className="w-full md:w-2/3 my-4">
+      <div className="w-full relative">
+        <IoIosArrowDropleftCircle
+          size={38}
+          className="
+            absolute left-0 top-1/2 transform -translate-y-1/2 text-3xl 
+            cursor-pointer
+            text-neutral-500 dark:text-neutral-700
+            hover:text-red-500 dark:hover:text-red-900
+            transition-colors duration-300"
+          onClick={handlePrev}
+        />
+        {/* Image Preview */}
         <Image
           src={images[activeIndex]}
           alt="Currently Active"
@@ -35,16 +62,27 @@ const Gallery: React.FC<GalleryProps> = ({ images }) => {
           height={500}
           className="w-full h-auto object-cover rounded-xl shadow"
         />
+        <IoIosArrowDroprightCircle
+          size={38}
+          className="
+            absolute right-0 top-1/2 transform -translate-y-1/2 text-3xl 
+            cursor-pointer
+            text-neutral-500 dark:text-neutral-700
+            hover:text-red-500 dark:hover:text-red-900
+            transition-colors duration-300"
+          onClick={handleNext}
+        />
       </div>
-      <FiArrowRightCircle
-        className="absolute right-0 top-1/2 transform -translate-y-1/2 text-3xl cursor-pointer"
-        onClick={handleNext}
-      />
-      <div className="flex flex-wrap justify-center gap-2">
+      <div className="flex flex-wrap justify-center gap-2 mt-4">
+        {/* Image List */}
         {images.map((image, idx) => (
           <div
             key={idx}
-            className="w-16 h-16 border border-gray-200 rounded overflow-hidden cursor-pointer hover:border-gray-400"
+            className={`w-16 h-16 ${
+              idx === activeIndex
+                ? "border-4 border-red-500 dark:border-red-400 hover:border-red-600 dark:hover:border-red-500"
+                : "border-2 border-neutral-200 dark:border-neutral-700 hover:border-neutral-400 dark:hover:border-red-900"
+            } rounded-lg overflow-hidden cursor-pointer `}
             onClick={() => setActiveIndex(idx)}
           >
             <Image
