5694af5a88527500651d1250d3c574036a7a3a10
Maruf Bepary <bepary71@gmail.com>
2023-07-09 23:34:19 +0100

    Added loading animation for all pages

---
+++ components/Atoms/LoadingAnimation.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { BounceLoader } from "react-spinners";
+
+/**
+ * Loading animation component which displays a loading animation.
+ * This is used on loading pages while content is being fetched.
+ *
+ * @returns (React.ReactNode): loading animation
+ */
+const LoadingAnimation = () => {
+  return (
+    <div
+      className="
+			rounded-lg 
+			w-full h-full 
+			flex items-center justify-center
+			min-h-[85vh]
+			"
+    >
+      <BounceLoader color="#ff0000" size={40} />
+    </div>
+  );
+};
+
+export default LoadingAnimation;
