65882c7a2979301649baf6813e92900cbd263949
Maruf Bepary <bepary71@gmail.com>
2023-07-17 13:19:53 +0100

    BUG FIX: on desktop clicking a button on the navbar would expand it - Instead of toggling the navbar (open if close and close if open), a close function is triggered instead

--- atoms/navbarStore.ts
+++ atoms/navbarStore.ts
@@ -3,6 +3,8 @@ import { create } from "zustand";
 interface NavbarStore {
   isOpen: boolean;
   toggle: () => void;
+  open: () => void;
+  close: () => void;
 }
 
 /**
@@ -11,9 +13,13 @@ interface NavbarStore {
  * Clicking the hamburger icon toggles the navbar (open or closed).
  * Clicking a link in the navbar minimises the navbar.
  * - `isOpen`: whether the navbar is open or not
- * - `toggle`: function to toggle the navbar
+ * - `toggle`: function to toggle the navbar (if open, close it; if closed, open it)
+ * - `open`: function to open the navbar
+ * - `close`: function to close the navbar
  */
 export const useNavbarStore = create<NavbarStore>((set) => ({
   isOpen: false, // initial navbar is closed
   toggle: () => set((state) => ({ isOpen: !state.isOpen })), // toggle navbar
+  open: () => set({ isOpen: true }), // open navbar
+  close: () => set({ isOpen: false }), // close navbar
 }));
