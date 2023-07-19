c4eaa2e922b051b422b52879757ba3dcd322f025
Maruf Bepary <bepary71@gmail.com>
2023-07-19 14:09:34 +0100

    Used button component and made into server component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,4 @@
-"use client";
-
-import { useRouter } from "next/navigation";
+import Button from "@/components/Atoms/Button";
 
 export const revalidate = 0; // page will not be cached
 
@@ -11,22 +9,17 @@ export const revalidate = 0; // page will not be cached
  * @returns (React.FC): PageMessage component with a 404 error message
  */
 export default function NotFound() {
-  const router = useRouter();
-
   return (
     <div className="flex flex-col items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 text-center min-h-[85vh]">
       <h1 className="text-5xl font-semibold text-red-500 dark:text-red-700 my-6">
         404: Page does not exist
       </h1>
-      <h2 className="text-neutral-900 dark:text-neutral-100 transition-colors duration-500 text-2xl font-medium">
+      <h2 className="text-neutral-900 dark:text-neutral-100 transition-colors duration-500 text-2xl font-medium mb-4">
         This page does not seem to exist. Navigate back to the home page.
       </h2>
-      <button
-        onClick={() => router.push("/")}
-        className="mt-10 px-6 py-2 text-neutral-100 font-semibold rounded-lg cursor-pointer bg-red-500  hover:bg-red-400 dark:bg-red-800  dark:hover:bg-red-900 transition-colors duration-500"
-      >
-        Go to Home Page
-      </button>
+      <Button onClick={"/"} variant="filled">
+        Home
+      </Button>
     </div>
   );
 }
