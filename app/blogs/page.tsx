191b533daeb5b8797b450258edefcadb5d6375e4
Maruf Bepary <bepary71@gmail.com>
2023-08-18 22:30:40 +0100

    Added search functionality for blogs

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,12 +1,13 @@
 import getBlogMetadata from "@/actions/getBlogMetadata";
+import { BlogList } from "@/components/Blogs/BlogList";
 import HeadingOne from "@/components/Text/HeadingOne";
-import BlogItem from "@/components/Blogs/BlogItem";
 
 /**
  * Displays a list of all blogs that can be opened.
+ * Also allows the user to search for blogs.
  * @returns (JSX.Element): page with all blogs
  */
-export default function Blog() {
+export default function BlogPage() {
   let blogMetadata = getBlogMetadata();
 
   // Filter blogs where display is 'true' or undefined
@@ -19,21 +20,8 @@ export default function Blog() {
       <section id="blogs">
         <div className="my-12 pb-12 md:pt-8 md:pb-48 animate-fadeIn animation-delay-2">
           <HeadingOne title="Blog" />
-          <div className="border-b border-gray-200 dark:border-neutral-600 mt-16" />
-          <div className="my-12 pb-12 md:pt-2 md:pb-36">
-            {/* Blog List */}
-            {blogMetadata.length > 0 ? (
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-14">
-                {blogMetadata.map((blog) => (
-                  <BlogItem key={blog.slug} {...blog} />
-                ))}
-              </div>
-            ) : (
-              <div className="flex justify-center min-w-full h-screen mt-14">
-                <h2 className="text-2xl font-bold">No blogs</h2>
-              </div>
-            )}
-          </div>
+
+          <BlogList blogs={blogMetadata} />
         </div>
       </section>
     </main>
