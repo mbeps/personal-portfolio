1464da56a8bb5aaafc85fe9a508308b2e7774868
Maruf Bepary <bepary71@gmail.com>
2023-09-12 20:16:05 +0100

    Added clear button to blog search

--- app/blogs/components/BlogList.tsx
+++ app/blogs/components/BlogList.tsx
@@ -1,11 +1,12 @@
 "use client";
 
+import Button from "@/components/Atoms/Button";
 import BlogItem from "@/components/Blogs/BlogItem";
 import SearchInput from "@/components/Inputs/SearchInput";
-import useDebounce from "@/hooks/useDebounce";
 import { BlogMetadata } from "@/types/blog";
 import Fuse from "fuse.js";
 import { useRouter, useSearchParams } from "next/navigation"; // Add this import for Next.js router
+import { MdClear } from "react-icons/md";
 
 interface BlogListProps {
   blogs: BlogMetadata[];
@@ -27,6 +28,12 @@ export const BlogList: React.FC<BlogListProps> = ({ blogs }) => {
     router.push(`/blogs/?search=${validatedSearch}`);
   };
 
+  const resetSearch = () => {
+    updateSearchTerm("");
+  };
+
+  const areFiltersApplied = searchTerm.length > 0;
+
   const options = {
     keys: ["title", "subtitle"],
     threshold: 0.3,
@@ -38,15 +45,44 @@ export const BlogList: React.FC<BlogListProps> = ({ blogs }) => {
     ? fuse.search(searchTerm).map((result) => result.item)
     : blogs;
 
+  const isClearDisabled = !searchTerm;
+
   return (
     <div className="my-12 pb-12 md:pt-2">
-      <div className="flex flex-col items-end px-0 md:px-2 pl-0 md:pl-6">
-        <div className="w-full md:w-1/2">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-2 px-0 md:px-2 pl-0">
+        <div className="md:col-span-2">
           <SearchInput
             searchTerm={searchTerm}
             updateSearchTerm={updateSearchTerm}
             placeholder="Search blog"
           />
+        </div>
+        <div className="md:col-span-1">
+          {/* Clear Search Button */}
+          <Button
+            variant="outlined"
+            onClick={resetSearch}
+            disabled={isClearDisabled} // Disabled if no search term exists
+            className={`
+              px-4 py-2 w-full
+              text-base font-medium text-neutral-700 dark:text-neutral-200 capitalize hover:text-neutral-700 dark:hover:text-neutral-200
+              rounded-xl
+              shadow-md hover:shadow-lg focus:shadow-lg
+              bg-neutral-100 dark:bg-neutral-800 
+              hover:bg-neutral-100 dark:hover:bg-neutral-800
+              border-2 border-transparent dark:border-transparent
+              hover:border-red-500 dark:hover:border-red-800
+              transition-all duration-500 ease-in-out
+            `}
+          >
+            <div className="flex items-center space-x-2">
+              <MdClear
+                fontSize={24}
+                className="text-neutral-700 dark:text-neutral-200"
+              />
+              <span>Clear Search</span>
+            </div>
+          </Button>
         </div>
       </div>
       <div className="border-b border-gray-200 dark:border-neutral-600 mt-16" />
