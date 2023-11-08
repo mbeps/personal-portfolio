4b2a74b05713268b53eac6e358291414536536eb
Maruf Bepary <bepary71@gmail.com>
2023-11-08 19:36:43 +0000

    Created toggle component

---
+++ components/Inputs/Toggle.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { Switch } from "@headlessui/react";
+
+export default function Toggle() {
+  const [enabled, setEnabled] = useState(false);
+
+  return (
+    <div className="py-16">
+      <Switch
+        checked={enabled}
+        onChange={setEnabled}
+        className={`${enabled ? "bg-green-700" : "bg-red-900"}
+          relative inline-flex h-[30px] w-[54px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white/75`}
+      >
+        <span className="sr-only">Use setting</span>
+        <span
+          aria-hidden="true"
+          className={`${enabled ? "translate-x-6" : "translate-x-0"}
+            pointer-events-none inline-block h-[26px] w-[26px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+        />
+      </Switch>
+    </div>
+  );
+}
