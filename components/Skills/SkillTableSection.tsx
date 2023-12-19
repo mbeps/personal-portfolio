b2e8a936db6c957e0b180404f74a1107c943bea6
Maruf Bepary <bepary71@gmail.com>
2023-12-19 14:16:34 +0000

    Allowing expanding and minimising skills - Display limited number of skills and groups - Display button to expand and minimise skills displayed

--- components/Skills/SkillTableSection.tsx
+++ components/Skills/SkillTableSection.tsx
@@ -1,8 +1,10 @@
-import Tag from "@/components/Tags/Tag";
+"use client";
+
 import HeadingFour from "@/components/Text/HeadingFour";
 import HeadingThree from "@/components/Text/HeadingThree";
 import { Skill } from "@/types/skills";
-import React from "react";
+import React, { useState } from "react";
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
 import SkillTag from "../Tags/SkillTag";
 
 interface SkillTableSectionProps {
@@ -14,25 +16,72 @@ const SkillTableSection: React.FC<SkillTableSectionProps> = ({
   skillCategories,
   title,
 }) => {
+  const [showAll, setShowAll] = useState(false);
   const categories = Object.entries(skillCategories);
   const shouldDisplayTitle = categories.length > 1;
+
+  const maxSkillCount = 12;
+  const maxGroupCount = 4;
+
+  let skillCount = 0;
+  let groupCount = 0;
+  const displayedSkills = showAll
+    ? categories
+    : categories.reduce((acc: [string, Skill[]][], [category, skills]) => {
+        if (skillCount < maxSkillCount && groupCount < maxGroupCount) {
+          const availableSlots = Math.min(
+            maxSkillCount - skillCount,
+            skills.length
+          );
+          acc.push([category, skills.slice(0, availableSlots)]);
+          skillCount += availableSlots;
+          groupCount++;
+        }
+        return acc;
+      }, []);
+
+  const totalSkillCount = categories.reduce(
+    (acc, [_, skills]) => acc + skills.length,
+    0
+  );
+
+  // Adjust the condition for showing the toggle button
+  const shouldShowToggleButton = totalSkillCount > skillCount || showAll;
+
+  const toggleShowAll = () => {
+    setShowAll(!showAll);
+  };
 
   return (
     <div className="mt-4 text-center md:text-left">
       <HeadingThree title={title} />
 
       <div className="gap-4">
-        {categories.map(([category, skills]) => (
+        {displayedSkills.map(([category, skills]) => (
           <div key={category} className="mb-6">
             {shouldDisplayTitle && <HeadingFour title={category} />}
             <div className="flex flex-wrap justify-center md:justify-start">
               {skills.map((skill, index) => (
-                <SkillTag key={index} skill={skill} />
+                <SkillTag key={skill.slug} skill={skill} />
               ))}
             </div>
           </div>
         ))}
       </div>
+
+      {shouldShowToggleButton && (
+        <button
+          className="mt-2 text-red-600 hover:text-red-800 flex items-center"
+          onClick={toggleShowAll}
+        >
+          {showAll ? "Show Less" : "Show More"}
+          {showAll ? (
+            <MdKeyboardArrowUp size={25} />
+          ) : (
+            <MdKeyboardArrowDown size={25} />
+          )}
+        </button>
+      )}
     </div>
   );
 };
