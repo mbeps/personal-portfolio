41fc3b7caae3428dcf7b5c2fbce27aa4723df8df
Maruf Bepary <bepary71@gmail.com>
2023-12-19 15:27:22 +0000

    Updated spacing for projects list

--- app/projects/components/ProjectListSection.tsx
+++ app/projects/components/ProjectListSection.tsx
@@ -1,6 +1,7 @@
 import Project from "@/types/projects";
 import ProjectItem from "@/components/ProjectItem/ProjectItem";
 import HeadingTwo from "@/components/Text/HeadingTwo";
+import stringToSlug from "@/actions/stringToSlug";
 
 interface ProjectsListSectionProps {
   groupedProjects: Record<string, Project[]>;
@@ -15,30 +16,31 @@ const ProjectsListSection: React.FC<ProjectsListSectionProps> = ({
   groupedProjects,
 }) => {
   return (
-    <div className="flex flex-col space-y-20 mt-14">
+    <div className="flex flex-col space-y-10 mt-14">
       {Object.keys(groupedProjects).length > 0 ? (
         Object.keys(groupedProjects).map(
           (type) =>
             type !== "All" && (
-              <section key={type} id={type.toLowerCase().replace(/\s+/g, "-")}>
-                <div className="flex flex-col space-y-20">
+              <section key={type} id={stringToSlug(type)}>
+                <div className="flex flex-col space-y-10">
                   <div className="border-b border-gray-200 dark:border-neutral-600 pb-2" />
-                  {/* Assuming HeadingTwo is a component you have for rendering titles */}
                   <HeadingTwo title={type} />
-                  {groupedProjects[type].map((project, idx) => (
-                    <div key={idx}>
-                      {/* Assuming ProjectItem is a component for individual project items */}
-                      <ProjectItem
-                        name={project.name}
-                        slug={project.slug}
-                        key={project.imageURL}
-                        description={project.description}
-                        imageURL={project.imageURL}
-                        repoURL={project.repoURL}
-                        siteURL={project.siteURL}
-                      />
-                    </div>
-                  ))}
+                  <div className="space-y-20">
+                    {groupedProjects[type].map((project, idx) => (
+                      <div key={idx}>
+                        {/* Assuming ProjectItem is a component for individual project items */}
+                        <ProjectItem
+                          name={project.name}
+                          slug={project.slug}
+                          key={project.imageURL}
+                          description={project.description}
+                          imageURL={project.imageURL}
+                          repoURL={project.repoURL}
+                          siteURL={project.siteURL}
+                        />
+                      </div>
+                    ))}
+                  </div>
                 </div>
               </section>
             )
