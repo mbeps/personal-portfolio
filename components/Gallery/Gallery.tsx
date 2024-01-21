016a5b89256ee1f08c1275f8ad8edb84177534f6
Maruf Bepary <bepary71@gmail.com>
2024-01-21 21:49:54 +0000

    Update gallery design - Made the tab fully rounded - Made the container outside the carousel so that it doesn't scroll

--- components/Gallery/Gallery.tsx
+++ components/Gallery/Gallery.tsx
@@ -67,7 +67,10 @@ const Gallery: React.FC<GalleryProps> = ({ images, videos }) => {
         <Tabs defaultValue="images" className="w-full">
           {/* Images */}
           <TabsContent value="images" className="w-full">
-            <Carousel setApi={setApi}>
+            <Carousel
+              setApi={setApi}
+              className="bg-neutral-100 dark:bg-neutral-950 rounded-xl"
+            >
               <CarouselContent>
                 {Array.from({ length: images?.length ?? 0 }).map((_, index) => (
                   <CarouselItem key={index}>
@@ -79,7 +82,13 @@ const Gallery: React.FC<GalleryProps> = ({ images, videos }) => {
                       width={2000}
                       height={1125}
                       priority
-                      className="w-full h-[60vh] object-contain rounded-xl bg-neutral-100 dark:bg-neutral-950 transition-colors duration-700 p-2"
+                      className="
+                        w-full h-[60vh] 
+                        object-contain 
+                        rounded-xl 
+                        
+                        transition-colors duration-700 
+                        p-2"
                     />
                   </CarouselItem>
                 ))}
@@ -97,7 +106,10 @@ const Gallery: React.FC<GalleryProps> = ({ images, videos }) => {
           </TabsContent>
           {/* Video Demos */}
           <TabsContent value="demo">
-            <Carousel setApi={setApi}>
+            <Carousel
+              setApi={setApi}
+              className="bg-neutral-100 dark:bg-neutral-950 rounded-xl"
+            >
               <CarouselContent>
                 {Array.from({ length: videos?.length ?? 0 }).map((_, index) => (
                   <CarouselItem key={index}>
@@ -121,28 +133,28 @@ const Gallery: React.FC<GalleryProps> = ({ images, videos }) => {
           </TabsContent>
 
           <div className="flex justify-center items-center">
-            <TabsList>
-              <TabsTrigger 
-                value="images" 
+            <TabsList className="rounded-full">
+              <TabsTrigger
+                value="images"
                 className="
                   flex flex-row space-x-1 
-                  text-neutral-700 dark:text-neutral-200 text-lg
-                  ">
-                    <LiaImageSolid fontSize={20}/>
-                    <span>
-                      Images
-                    </span>
+                  text-neutral-700 dark:text-neutral-200 text-md
+                  rounded-full
+                  "
+              >
+                <LiaImageSolid fontSize={20} />
+                <span>Images</span>
               </TabsTrigger>
-              <TabsTrigger 
-                value="demo" 
+              <TabsTrigger
+                value="demo"
                 className="
                   flex flex-row space-x-1 
-                  text-neutral-700 dark:text-neutral-200 text-lg
-                  ">
-                    <LiaVideoSolid fontSize={20}/>
-                    <span>
-                      Videos
-                    </span>
+                  text-neutral-700 dark:text-neutral-200 text-md
+                  rounded-full
+                  "
+              >
+                <LiaVideoSolid fontSize={20} />
+                <span>Videos</span>
               </TabsTrigger>
             </TabsList>
           </div>
