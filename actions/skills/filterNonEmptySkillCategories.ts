62028ddf16fcc2ae5ff04d6c53a41d3eab5285fb
Maruf Bepary <bepary71@gmail.com>
2024-01-30 09:50:22 +0000

    REFACTOR: Minimised code repatition and created function to filtering skills

---
+++ actions/skills/filterNonEmptySkillCategories.ts
@@ -0,0 +1,5 @@
+import GroupedSkillsCategoriesInterface from "@/interfaces/skills/GroupedSkillsInterface";
+
+export default function filterNonEmptySkillCategories(groupedSkills: GroupedSkillsCategoriesInterface[]): GroupedSkillsCategoriesInterface[] {
+	return groupedSkills.filter(({ skillCategories }) => skillCategories.length > 0);
+}
