463358df8a298ab8c06237031c0da7cf2f9ab394
Maruf Bepary <bepary71@gmail.com>
2024-02-05 11:05:26 +0000

    Making blog item height more consistent on tablet

--- components/Blogs/BlogItem.tsx
+++ components/Blogs/BlogItem.tsx
@@ -15,17 +15,17 @@ const BlogItem: React.FC<BlogInterface> = (props) => {
     <Link href={`${basePath}/${props.slug}`}>
       <div
         className="
-        flex flex-col 
-        min-h-[180px]
-        cursor-pointer
-        bg-neutral-100 dark:bg-neutral-800
-        md:hover:bg-neutral-200 md:dark:hover:bg-red-950
-        p-4 
-        rounded-xl 
-        shadow-md md:hover:shadow-lg
-        transform md:hover:scale-105
-        animate-slideUpCubiBezier animation-delay-2
-        transition-all duration-500 ease-in-out"
+          flex flex-col
+          min-h-[180px] h-full max-h-[280px]
+          cursor-pointer
+          bg-neutral-100 dark:bg-neutral-800
+          md:hover:bg-neutral-200 md:dark:hover:bg-red-950
+          p-4
+          rounded-xl
+          shadow-md md:hover:shadow-lg
+          transform md:hover:scale-105
+          animate-slideUpCubiBezier animation-delay-2
+          transition-all duration-500 ease-in-out"
       >
         <h2 className=" text-xl font-bold  mb-4 text-neutral-900 dark:text-neutral-100">
           {props.name}
