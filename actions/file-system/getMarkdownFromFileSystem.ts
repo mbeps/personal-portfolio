0d768c5d11c915367af15c5860bfb1841067859b
Maruf Bepary <bepary71@gmail.com>
2024-03-27 15:34:02 +0000

    DOC/REFACTOR: Documented file system related functions and changed them to use function notation instead of const notation

--- actions/file-system/getMarkdownFromFileSystem.ts
+++ actions/file-system/getMarkdownFromFileSystem.ts
@@ -2,22 +2,21 @@ import matter from "gray-matter";
 import fs from "fs";
 
 /**
- * Reads a markdown file from the file system and parses it using gray-matter
+ * Reads a markdown file from the file system and parses it using gray-matter.
+ * This can then be used to extract the frontmatter and content and display it in the UI.
  *
- * @param filePath (string): The path to the markdown file
- * @returns (matter.GrayMatterFile<string> | null): The parsed markdown file or null if an error occurred
+ * @param filePath: The path to the markdown file
+ * @returns: The parsed markdown file or null if an error occurred
  */
-const getMarkdownFromFileSystem = (
+export default function getMarkdownFromFileSystem(
   filePath: string
-): matter.GrayMatterFile<string> | null => {
+): matter.GrayMatterFile<string> | null {
   try {
-    const content = fs.readFileSync(filePath, "utf8");
-    const matterResult = matter(content);
+    const content: string = fs.readFileSync(filePath, "utf8");
+    const matterResult: matter.GrayMatterFile<string> = matter(content);
     return matterResult;
   } catch (error) {
     // console.log(`Error reading markdown file ${filePath}:`, error);
     return null;
   }
-};
-
-export default getMarkdownFromFileSystem;
+}
