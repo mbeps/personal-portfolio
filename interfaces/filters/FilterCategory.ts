c2823b731de9a26a03a0125dcc0294fec377d007
Maruf Bepary <bepary71@gmail.com>
2024-03-27 13:32:55 +0000

    DOC: Documented filter interfaces

--- interfaces/filters/FilterCategory.ts
+++ interfaces/filters/FilterCategory.ts
@@ -1,8 +1,19 @@
 import FilterOption from "./FilterOption";
 
+/**
+ * Interface representing a category of filters.
+ * This interface stores all the necessary metadata to display and manage a category of filters.
+ * The fields are:
+ * - `sectionName` which is the name of the section grouping the filter options.
+ * - `urlParam` which is the url parameter which keeps track of the selected filter option.
+ * - `selectedValue` which is the currently selected filter option.
+ * - `options` which is a list of all options that can be selected.
+ *
+ * @requires {@link FilterOption} to represent the list of options that can be selected.
+ */
 export default interface FilterCategory {
-  sectionName: string;
-  urlParam: string;
-  selectedValue: string;
-  options: FilterOption[];
+  sectionName: string; // name of the section groping the filter options
+  urlParam: string; // url parameter which keeps track of the selected filter option
+  selectedValue: string; // currently selected filter option
+  options: FilterOption[]; // list of all options that can be selected
 }
--- interfaces/filters/FilterOption.ts
+++ interfaces/filters/FilterOption.ts
@@ -1,3 +1,9 @@
+/**
+ * Interface representing a single filter option.
+ * This interface contains the field:
+ * - `entryName` which is the name of the filter option in a readable format.
+ * - `slug` which is the unique identifier of the filter option used in the URL.
+ */
 export default interface FilterOption {
   entryName: string;
   slug: string;
