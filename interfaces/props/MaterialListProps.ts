8c45eebee25579c09ce46ac484a17f118ae722c4
Maruf Bepary <bepary71@gmail.com>
2024-03-27 13:36:15 +0000

    DOC: Documented props interfaces

--- interfaces/props/MaterialListProps.ts
+++ interfaces/props/MaterialListProps.ts
@@ -1,5 +1,12 @@
 import MaterialGroupInterface from "../material/MaterialGroupInterface";
 
+/**
+ * Interface representing the props of the MaterialList components.
+ * This interface stores the list of materials grouped by the `MaterialGroupInterface` interface which itself is a groping of materials.
+ * This is used to displays grouped materials iteratively my material list components.
+ *
+ * @requires {@link MaterialGroupInterface} to represent the list of materials that are grouped together.
+ */
 export default interface MaterialListProps {
   groupedMaterial: MaterialGroupInterface[];
 }
