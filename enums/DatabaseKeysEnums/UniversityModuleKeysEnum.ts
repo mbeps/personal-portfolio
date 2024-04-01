3bb19d441755b18295517c6200a126adcdfe1977
Maruf Bepary <bepary71@gmail.com>
2024-04-01 18:35:01 +0100

    FEATURES: Created enums needed for the databases

---
+++ enums/DatabaseKeysEnums/UniversityModuleKeysEnum.ts
@@ -0,0 +1,26 @@
+enum UniversityModuleKeysEnum {
+  RHUL_ObjectOrientedProgramming1 = "CS1811",
+  RHUL_ObjectOrientedProgramming2 = "CS1812",
+  RHUL_ProgrammingLaboratory = "CS1822",
+  RHUL_InternetServices = "CS1840",
+  RHUL_MathematicalStructures = "CS1860",
+  RHUL_MachineFundamentals = "CS1870",
+  RHUL_SoftwareDesign = "CS1890",
+  RHUL_SoftwareEngineering = "CS2800",
+  RHUL_TeamProject = "CS2810",
+  RHUL_OperatingSystems = "CS2850",
+  RHUL_Databases = "CS2855",
+  RHUL_AlgorithmsAndComplexity = "CS2860",
+  RHUL_MultidimensionalDataProcessing = "CS2900",
+  RHUL_ArtificialIntelligence = "CS2910",
+  RHUL_IntroductionToInformationSecurity = "IY2760",
+  RHUL_UserCentredDesign = "CS3001",
+  RHUL_ITProjectManagement = "CS3003",
+  RHUL_FunctionalProgramming = "CS3510",
+  RHUL_FinalYearProject = "CS3821",
+  RHUL_MachineLearning = "CS3920",
+  RHUL_ComputationalFinance = "CS3930",
+  RHUL_SecurityManagement = "IY3501",
+}
+
+export default UniversityModuleKeysEnum;
