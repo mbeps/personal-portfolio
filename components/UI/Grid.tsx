56cffc4b9e04e0005dff9c5e220ecfdbbb3e8cab
Maruf Bepary <bepary71@gmail.com>
2024-04-06 17:34:24 +0100

    BUG FIX: Grid component wasn't using default gap

--- components/UI/Grid.tsx
+++ components/UI/Grid.tsx
@@ -14,33 +14,30 @@ interface GridProps {
  * @param gap The gap between grid items, default is 5
  * @returns All the items in a grid layout
  */
-const Grid: React.FC<GridProps> = ({ items, gap = 5 }) => {
-  const isOddTotal = items.length % 2 !== 0;
+const Grid: React.FC<GridProps> = ({ items, gap = 20 }) => {
+  const isOddTotal: boolean = items.length % 2 !== 0;
+
+  // Define grid style dynamically based on the gap prop
+  const gridStyle = {
+    gridGap: `${gap}px`,
+  };
 
   return (
     <div
-      className={`
-        grid grid-cols-1 md:grid-cols-2
-        gap-${gap} md:gap-x-${gap}
-      `}
+      className="grid grid-cols-1 md:grid-cols-2"
+      style={gridStyle} // Apply the dynamic grid style here
     >
       {items.map((item, idx) => {
         const isLastItem = idx === items.length - 1;
-        if (isLastItem && isOddTotal) {
-          // last item on grid
-          return (
-            <div key={idx} className={`md:col-span-2 flex justify-center`}>
-              <div className="w-full md:w-1/2">{item}</div>
-            </div>
-          );
-        } else {
-          // items on grid
-          return (
-            <div className="w-full" key={idx}>
-              {item}
-            </div>
-          );
-        }
+        return isLastItem && isOddTotal ? (
+          <div key={idx} className="md:col-span-2 flex justify-center">
+            <div className="w-full md:w-1/2">{item}</div>
+          </div>
+        ) : (
+          <div className="w-full" key={idx}>
+            {item}
+          </div>
+        );
       })}
     </div>
   );
