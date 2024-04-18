3c646bbb1584cfa95d902424488f5210cd225ffe
Maruf Bepary <bepary71@gmail.com>
2024-04-18 12:50:15 +0100

    Improved the design of the collapse button for the material list

--- components/MaterialLists/MaterialList.tsx
+++ components/MaterialLists/MaterialList.tsx
@@ -23,25 +23,28 @@ const MaterialList: React.FC<MaterialTabsProps> = ({
   sectionName,
 }) => {
   return isCollapsible ? (
-    <Accordion
-      type="single"
-      collapsible
-      className="
-        border-t-2 border-neutral-200 dark:border-neutral-700
-        p-3 mt-5"
-    >
-      <AccordionItem value="item-1">
-        <AccordionTrigger>
+    <Accordion type="single" collapsible className="mt-16">
+      <AccordionItem value="item-1" className="border-none">
+        <AccordionTrigger
+          className="
+            rounded-xl px-3
+            border
+            border-neutral-200 dark:border-neutral-700
+            hover:border-neutral-300 dark:hover:border-neutral-600
+            bg-neutral-50 dark:bg-neutral-950
+            shadow-sm hover:shadow-md
+            transition-all duration-500 ease-in-out"
+        >
           <p
             className="
-              text-lg 
+              text-lg font-semibold text-left
               text-neutral-700 dark:text-neutral-300
               "
           >
             {`List of material directly related to ${sectionName}`}
           </p>
         </AccordionTrigger>
-        <AccordionContent>
+        <AccordionContent className="mt-4">
           <MaterialTab materialKeys={materialKeys} defaultTab={defaultTab} />
         </AccordionContent>
       </AccordionItem>
