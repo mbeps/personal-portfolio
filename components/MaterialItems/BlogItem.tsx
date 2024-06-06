4f287fb81604ffa2442a7d3ab2fa1fa7787b8b0f
Maruf Bepary <bepary71@gmail.com>
2024-06-06 22:38:29 +0100

    Improved blog item borders - No hover border more prominent - Added hover border

--- components/MaterialItems/BlogItem.tsx
+++ components/MaterialItems/BlogItem.tsx
@@ -24,17 +24,18 @@ const BlogItem: React.FC<BlogItemProps> = ({ blogKey }) => {
       <Link href={`${basePath}/${blogKey}`}>
         <div
           className="
-          flex flex-col
-          min-h-[180px] h-full max-h-[280px]
-          cursor-pointer
-          bg-neutral-100 dark:bg-neutral-800
-          md:hover:bg-neutral-200 md:dark:hover:bg-red-950
-          p-4
-          border border-neutral-200 dark:border-neutral-800
-          rounded-xl
-          shadow-md md:hover:shadow-lg
-          transform md:hover:scale-105
-          transition-all duration-500 ease-in-out"
+            flex flex-col
+            min-h-[180px] h-full max-h-[280px]
+            cursor-pointer
+            bg-neutral-100 dark:bg-neutral-800
+            md:hover:bg-neutral-200 md:dark:hover:bg-red-950
+            p-4
+            border border-neutral-200 dark:border-neutral-700
+            hover:border-neutral-400 dark:hover:border-red-500
+            rounded-xl
+            shadow-md md:hover:shadow-lg
+            transform md:hover:scale-105
+            transition-all duration-500 ease-in-out"
         >
           <h2 className=" text-xl font-bold  mb-4 text-neutral-900 dark:text-neutral-100">
             {blogData.name}
