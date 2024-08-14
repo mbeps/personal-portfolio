f94e5503a63e6297c0484ee5033f7c68f47d5991
Maruf Bepary <bepary71@gmail.com>
2024-08-14 11:58:32 +0100

    Clicking button opens search dialog - Values inside are hard coded

--- components/GlobalSearch/SearchButton.tsx
+++ components/GlobalSearch/SearchButton.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+} from "@/components/shadcn/ui/command";
 import { RiSearchLine } from "react-icons/ri";
 
 const SearchButton: React.FC = () => {
+  const [open, setOpen] = useState(false);
+
   const baseButtonClass =
     "group p-2.5 rounded-full transition-colors duration-1000";
   const darkButtonClass = "dark:bg-black dark:hover:bg-white";
@@ -12,14 +24,29 @@ const SearchButton: React.FC = () => {
   const lightIconClass = "text-black group-hover:text-white";
 
   return (
-    <button
-      className={`${baseButtonClass} ${darkButtonClass} ${lightButtonClass}`}
-    >
-      <RiSearchLine
-        size={25}
-        className={`${baseIconClass} ${darkIconClass} ${lightIconClass}`}
-      />
-    </button>
+    <>
+      <button
+        className={`${baseButtonClass} ${darkButtonClass} ${lightButtonClass}`}
+        onClick={() => setOpen((open) => !open)}
+      >
+        <RiSearchLine
+          size={25}
+          className={`${baseIconClass} ${darkIconClass} ${lightIconClass}`}
+        />
+      </button>
+
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>Calendar</CommandItem>
+            <CommandItem>Search Emoji</CommandItem>
+            <CommandItem>Calculator</CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </>
   );
 };
 
