b16810110e0eaf00981cc814edb86c7f9df8d9df
Maruf Bepary <bepary71@gmail.com>
2024-08-30 13:30:13 +0100

    REVERT: Course item to old large image on top

--- components/MaterialItems/CourseItem.tsx
+++ components/MaterialItems/CourseItem.tsx
@@ -26,27 +26,27 @@ const CourseItem: React.FC<CourseItemProps> = ({ courseKey }) => {
         bg-neutral-100 dark:bg-neutral-800
         border border-neutral-300 dark:border-neutral-700
         shadow-md
-        p-3 rounded-xl
+        p-3 lg:p-6 rounded-xl
         transition-colors duration-700
-        flex flex-col lg:grid lg:grid-cols-3
+        flex flex-col
         animate-slideUpCubiBezier animation-delay-2
         h-full w-full
       "
     >
       {/* Certificate Image */}
       {courseData.certificate && (
-        <Link href={`education/${courseKey}`} className="lg:col-span-1">
+        <Link href={`education/${courseKey}`}>
           <div
             className="
-              flex justify-center
-              rounded-xl
-              transform md:hover:scale-105
-              shadow-sm md:hover:shadow-lg
-              transition-all duration-500 ease-in-out
-              mb-6
-              w-full
-              overflow-hidden
-            "
+            flex justify-center
+            rounded-xl
+            transform md:hover:scale-105
+            shadow-sm md:hover:shadow-lg
+            transition-all duration-500 ease-in-out
+            mb-6
+            w-full
+            overflow-hidden
+        "
           >
             <AspectRatio ratio={1 / 1.4} className="overflow-hidden relative">
               <Image
@@ -60,7 +60,7 @@ const CourseItem: React.FC<CourseItemProps> = ({ courseKey }) => {
                   rounded-xl
                   cursor-pointer
                   object-cover
-                "
+              "
               />
             </AspectRatio>
           </div>
@@ -69,33 +69,28 @@ const CourseItem: React.FC<CourseItemProps> = ({ courseKey }) => {
 
       <div
         className="
-          flex flex-col 
-          gap-5 px-4 py-4
-          lg:col-span-2
-          space-y-4
-        "
+        flex flex-col 
+        gap-5 px-4 py-4"
       >
-        <div className="lg:w-full lg:text-left">
-          {/* Certificate Title */}
-          <Link href={`education/${courseKey}`}>
-            <h1
-              className="
-                text-3xl md:text-4xl font-bold text-center lg:text-left
-                md:hover:text-red-500 md:dark:hover:text-red-800
-                transition-colors duration-700 ease-in-out
+        {/* Certificate Title */}
+        <Link href={`education/${courseKey}`}>
+          <h1
+            className="
+              text-3xl md:text-4xl font-bold text-center 
+              md:hover:text-red-500 md:dark:hover:text-red-800
+              transition-colors duration-700 ease-in-out
               "
-            >
-              {courseData.name}
-            </h1>
-          </Link>
+          >
+            {courseData.name}
+          </h1>
+        </Link>
 
-          <p className="text-xl text-center lg:text-left leading-7 text-neutral-600 dark:text-neutral-400">
-            {courseData.grade}
-          </p>
+        <p className="text-xl text-center leading-7 text-neutral-600 dark:text-neutral-400">
+          {courseData.grade}
+        </p>
 
-          <div className="w-full flex justify-center lg:justify-start">
-            <Tag>{courseData.university}</Tag>
-          </div>
+        <div className="w-full flex justify-center">
+          <Tag>{courseData.university}</Tag>
         </div>
       </div>
     </div>
