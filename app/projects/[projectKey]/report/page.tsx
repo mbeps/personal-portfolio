ca8c98ae630cf7a4753b5a4b89bfb400e00ff867
Maruf Bepary <bepary71@gmail.com>
2025-01-30 22:14:53 +0000

    Build fix due to small mistake in project report page

--- app/projects/[projectKey]/report/page.tsx
+++ app/projects/[projectKey]/report/page.tsx
@@ -9,45 +9,12 @@ import { Metadata, ResolvingMetadata } from "next";
 import { notFound } from "next/navigation";
 import React from "react";
 
+// Update the type definitions
 type Params = { projectKey: string };
 
 type PageProps = {
-  params: Params;
+  params: Promise<Params>;
 };
-
-/**
- * Generates the metadata for the project page.
- * This includes the title and description of the page.
- * This is used for SEO purposes.
- *
- * @param props The props for the project page.
- * @param parent The parent metadata that is being resolved.
- * @returns The metadata for the project page.
- * @see https://nextjs.org/docs/app/building-your-application/optimizing/metadata
- */
-export async function generateMetadata(
-  props: { params: Params },
-  parent: ResolvingMetadata
-): Promise<Metadata | undefined> {
-  const resolvedParams = await props.params;
-  const projectKey: string = resolvedParams.projectKey;
-  const project: ProjectInterface = projectDatabaseMap[projectKey];
-
-  if (!project) {
-    notFound();
-  }
-
-  if (!project.archived) {
-    return {
-      title: `${developerName} - Project Report: ${project?.name}`,
-      description: `Report for the project ${project?.name}`,
-      category: `${PROJECTS_PAGE.label}`,
-      creator: developerName,
-    };
-  }
-
-  return undefined;
-}
 
 /**
  * Page that displays the report for a project if it exists.
@@ -55,14 +22,11 @@ export async function generateMetadata(
  * @param props Key of the parent project page
  * @returns Page that displays the report for a project
  */
-const ModulePage: React.FC<PageProps> = async ({ params }) => {
-  const projectKey: string = params.projectKey;
+const ModulePage = async ({ params }: PageProps) => {
+  const resolvedParams = await params;
+  const projectKey: string = resolvedParams.projectKey;
   const projectData: ProjectInterface = projectDatabaseMap[projectKey];
 
-  /**
-   * Get the blog content from the file system.
-   * This is used to display the features and blog sections.
-   */
   const reportBlog: string | undefined = getMarkdownFromFileSystem(
     `public${PROJECTS_PAGE.path}/${projectKey}/blog.md`
   )?.content;
@@ -88,5 +52,39 @@ const ModulePage: React.FC<PageProps> = async ({ params }) => {
     </main>
   );
 };
+
+/**
+ * Generates the metadata for the project report page.
+ * This includes the title and description of the page.
+ * This is used for SEO purposes.
+ *
+ * @param props The props for the project report page.
+ * @param parent The parent metadata that is being resolved.
+ * @returns The metadata for the project page.
+ * @see https://nextjs.org/docs/app/building-your-application/optimizing/metadata
+ */
+export async function generateMetadata(
+  { params }: { params: Promise<Params> },
+  parent: ResolvingMetadata
+): Promise<Metadata | undefined> {
+  const resolvedParams = await params;
+  const projectKey: string = resolvedParams.projectKey;
+  const project: ProjectInterface = projectDatabaseMap[projectKey];
+
+  if (!project) {
+    notFound();
+  }
+
+  if (!project.archived) {
+    return {
+      title: `${developerName} - Project Report: ${project?.name}`,
+      description: `Report for the project ${project?.name}`,
+      category: `${PROJECTS_PAGE.label}`,
+      creator: developerName,
+    };
+  }
+
+  return undefined;
+}
 
 export default ModulePage;
